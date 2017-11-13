b13e0a327966b34618d5693c0498efe44236371f
I327686 <atul.gupta02@sap.com>
2017-11-13 11:50:34 +0530

    Date preprocessing cont. + Simple Linear Regression

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -14,7 +14,7 @@ import pandas as pd
 dataset = pd.read_csv("Data.csv")
 
 X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 3].values
 
 #missing data
 from sklearn.preprocessing import Imputer
@@ -28,6 +28,15 @@ labelEncoder_X = LabelEncoder()
 X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
 oneHotEncoder = OneHotEncoder(categorical_features = [0])
 X = oneHotEncoder.fit_transform(X).toarray()
-labelEncoder_Y = LabelEncoder()
-Y = labelEncoder_Y.fit_transform(Y)
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
 
+#splitting dataset into training & test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_tranform(X_train)
+X_test = sc_X.transform(X_test)
